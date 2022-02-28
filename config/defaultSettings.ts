c9f6d9227c1384914cf6ff17b5237dc68b31034a
Sherry <sherryyang99@163.com>
2022-02-28 10:03:28 +0800

    add

---
+++ config/defaultSettings.ts
@@ -0,0 +1,20 @@
+import { Settings as LayoutSettings } from '@ant-design/pro-layout';
+
+const Settings: LayoutSettings & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#e5c875',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: 'ballet',
+  pwa: false,
+  iconfontUrl: '',
+};
+
+export default Settings;
